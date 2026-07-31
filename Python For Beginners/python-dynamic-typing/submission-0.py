5a1f38a90b47660d87e4b43d34817000694d48e1
villemairep <123842424+villemairep@users.noreply.github.com>
2026-07-31 15:01:26 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable=10
+print(type(variable))
+variable=10.5
+print(type(variable))
+variable=True
+print(type(variable))
+variable="Hello"
+print(type(variable))
+variable=[1,2,3]
+print(type(variable))
